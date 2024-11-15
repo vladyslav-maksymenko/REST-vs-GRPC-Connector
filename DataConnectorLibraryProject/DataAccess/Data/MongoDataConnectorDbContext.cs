8a0d6e1d789248744ca3f4482a0843cd4ddf9afb
vladyslav-maksymenko <86008805+vladyslav-maksymenko@users.noreply.github.com>
2024-11-15 12:36:44 +0200

    First seup mongo DB

--- DataConnectorLibraryProject/DataAccess/Data/MongoDataConnectorDbContext.cs
+++ DataConnectorLibraryProject/DataAccess/Data/MongoDataConnectorDbContext.cs
@@ -1,9 +1,36 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using DataConnectorLibraryProject.Models;
+using Microsoft.EntityFrameworkCore;
+using MongoDB.EntityFrameworkCore.Extensions;
 
 namespace DataConnectorLibraryProject.DataAccess.Data
 {
     public class MongoDataConnectorDbContext : DbContext
     {
+        public MongoDataConnectorDbContext(DbContextOptions<MongoDataConnectorDbContext> options) : base(options)
+        {
+        }
+        
+        internal DbSet<Customer> Customers { get; set; }
+        internal DbSet<Employee> Employees { get; set; }
+        internal DbSet<Equipment> Equipments { get; set; }
+        internal DbSet<Performer> Performers { get; set; }
+        internal DbSet<Position> Positions { get; set; }
+        internal DbSet<ProvidedService> ProvidedServices { get; set; }
+        internal DbSet<Service> Services { get; set; }
+        internal DbSet<TypeVehicle> TypeVehicles { get; set; }
+        internal DbSet<Vehicle> Vehicles { get; set; }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Customer>().ToCollection("customer");
+            modelBuilder.Entity<Employee>().ToCollection("employee");
+            modelBuilder.Entity<Equipment>().ToCollection("equipment");
+            modelBuilder.Entity<Performer>().ToCollection("performer");
+            modelBuilder.Entity<Position>().ToCollection("position");
+            modelBuilder.Entity<ProvidedService>().ToCollection("providedService");
+            modelBuilder.Entity<Service>().ToCollection("service");
+            modelBuilder.Entity<TypeVehicle>().ToCollection("typeVehicle");
+            modelBuilder.Entity<Vehicle>().ToCollection("vehicle");
+        }
     }
 }
---
+++ DataConnectorLibraryProject/DataAccess/MongoDataConnectorDbContextFactory.cs
@@ -0,0 +1,30 @@
+using DataConnectorLibraryProject.DataAccess.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+
+namespace DataConnectorLibraryProject.DataAccess
+{
+    internal class MongoDataConnectorDbContextFactory : IDesignTimeDbContextFactory<MongoDataConnectorDbContext>
+    {
+        public MongoDataConnectorDbContext CreateDbContext(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var mongoConnectionString = configuration.GetSection("MongoDBSettings:AtlasURI").Value;
+            var mongoClient = new MongoClient(mongoConnectionString);
+            
+            var database = mongoClient.GetDatabase(configuration["MongoDBSettings:DatabaseName"]);
+
+            var options = new DbContextOptionsBuilder<MongoDataConnectorDbContext>()
+                .UseMongoDB(mongoClient, database.DatabaseNamespace.DatabaseName)
+                .Options;
+
+            return new MongoDataConnectorDbContext(options);
+        }
+    }
+}
